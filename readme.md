3c3be22c4a62203580aabd3cde98ecb0958bc278
Grigoriy Dobryakov <gdobryakov@bekitzur.com>
2019-07-30 14:40:29 +0300

    add allure reporting

---
+++ readme.md
@@ -0,0 +1,2 @@
+https://webdriver.io/docs/selectors.html
+https://webdriver.io/docs/allure-reporter.html
--- run-me.sh
+++ run-me.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
 ./node_modules/.bin/wdio wdio.conf.js
+./node_modules/allure-commandline/bin/allure generate ./allure-results/
