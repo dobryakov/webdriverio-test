75dcb1991ae6901d30ad4af449f5658a4d505312
Grigoriy Dobryakov <gdobryakov@bekitzur.com>
2019-07-31 12:27:48 +0300

    require allure reporter

--- test/specs/basic.js
+++ test/specs/basic.js
@@ -1,8 +1,16 @@
 const assert = require('assert');
+const allureReporter = require('@wdio/allure-reporter').default;
 
 describe('webdriver.io page', () => {
 
+    it('get token', () => {
+        //browser.url('https://hr-dev.bekitzur.com/api/users/login');
+        //console.log(browser);
+    });
+
     it('should have the right title', () => {
+
+        allureReporter.addFeature('Feature Title');
 
         browser.url('https://webdriver.io');
         const title = browser.getTitle();
